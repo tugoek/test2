9cfaf803141193deef000130ddad17c7c4d7b289
Tuna Goek <tuna.goek@capgemini.com>
2023-04-18 16:18:52 +0200

    test

---
+++ src/main/java/de/tuna/test/TestApplication23.java
@@ -0,0 +1,13 @@
+package de.tuna.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication23{
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication23.class, args);
+	}
+
+}
